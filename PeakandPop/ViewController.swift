61bf5fe7bfc71180947f5bf7e5c77b599704e092
Admin <admin@Admins-Mac.local>
2017-09-27 14:03:51 +0600

    This is the init commit to GitHub Remotely

--- PeakandPop/ViewController.swift
+++ PeakandPop/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIViewControllerPreviewingDelegate {
+    
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+        let preview = storyboard?.instantiateViewController(withIdentifier: "view2")
+        return preview
+    }
+    
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, commit viewControllerToCommit: UIViewController) {
+        let finalview = storyboard?.instantiateViewController(withIdentifier: "view3")
+        show(finalview!, sender: self)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if(traitCollection.forceTouchCapability == UIForceTouchCapability.available){
+            registerForPreviewing(with: self, sourceView: view)
+        }
+        else{
+            print("it's compatable")
+        }
     }
 
     override func didReceiveMemoryWarning() {
